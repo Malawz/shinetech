ed65fb42c7a8071a832bbec0613ac0a6c2ced162
Malaw Manandhar <malaw.mndr@gmail.com>
2017-07-05 10:28:59 +0545

    Testing

---
+++ about.php
@@ -0,0 +1,94 @@
+<?php include('header.php'); ?>
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap" style="padding-top:10px">
+				<div class="">
+					<!-- <a href="https://www.youtube.com/watch?v=LuEAVdpny2U" data-lightbox="iframe" class="border-radius">
+						<img src="images/img-video.jpg" alt="Image">
+						<span class="i-overlay"><img src="images/icons/play.png" alt="Play"></span>
+					</a>-->
+					<ul class="slider">
+<li><a href="images/slides/1.jpg" data-fancybox="gallery"><img src="images/slides/1.jpg" alt="Image"></a></li>
+<li><a href="images/slides/2.jpg" data-fancybox="gallery"><img src="images/slides/2.jpg" alt="Image"></a></li>
+<li><a href="images/slides/3.jpg" data-fancybox="gallery"><img src="images/slides/3.jpg" alt="Image"></a></li>
+<li><a href="images/slides/4.jpg" data-fancybox="gallery"><img src="images/slides/4.jpg" alt="Image"></a></li>
+<li><a href="images/slides/5.jpg" data-fancybox="gallery"><img src="images/slides/5.jpg" alt="Image"></a></li>
+<li><a href="images/slides/6.jpg" data-fancybox="gallery"><img src="images/slides/6.jpg" alt="Image"></a></li>
+					</ul>
+				</div>
+				<div class="container clearfix">
+
+
+
+
+
+
+
+
+
+
+
+
+
+					<div class="col-md-12 nobottommargin">
+
+						<div class="heading-block">
+							<h2>About Shinetech</h2>
+						</div>
+						<div class="col-divide-2">
+
+							<p>At ShineTech Electrical we take great pride in our workmanship, and it shows. Our commitment to reliability, quality and value extends throughout our entire business operation. We are confident that we can successfully meet your expectations of a modern electrical contractor.</p>
+	<p>Whether you require an Electrical Installation Condition Report (EICR) or you want to upgrade or replace fuse box, or you require a new installation or re-wiring, we are on hand. We offer routine and emergency services across Southeast London and you can call on us whenever you need reliable electrical advice and assistance.</p>
+	<p>
+	As a company ShineTech Electrical, our main priority is the quality of our work and customer satisfaction. We look forward to assisting you with your electrical jobs.</p>
+	<p>
+	We are Electrical Contractors that specialises in fault finding. We have all the testing equipment which helps us find faults quickly and efficiently which keeps the cost of the job below expectation.</p>
+	<p>
+	Although we are relatively young company, within this short time we have gained a reputation of being the best service provider in the local community.</p>
+	<p>
+	Contact us on 0208 317 4933 for expert electrical services. We would be delighted to offer you a competitive quote to consider at your leisure.
+</p>
+						</div>
+						<br></br>
+						<br></br>
+
+
+
+
+
+
+
+
+					</div>
+
+
+
+				</div>
+
+
+
+
+
+
+
+
+
+
+
+				<a class="button button-full center tright footer-stick">
+					<div class="container clearfix">
+						24/7 Emergency
+Electricians
+<!-- <strong>Book Now</strong>
+ <i class="icon-caret-right" style="top:4px;"></i>-->
+					</div>
+				</a>
+
+			</div>
+
+		</section><!-- #content end -->
+
+	<?php include('footer.php'); ?>
